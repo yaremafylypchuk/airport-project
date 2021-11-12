5cc70514f8aac2e3241a9a7b20c326d87ab23250
yaremafylypchuk <57004357+yaremafylypchuk@users.noreply.github.com>
2021-11-12 22:53:12 +0200

    #1 Extended Person with the necessary properties

--- airport/airport-pojo-bl/src/main/java/helsinki/personnel/Person.java
+++ airport/airport-pojo-bl/src/main/java/helsinki/personnel/Person.java
@@ -1,10 +1,13 @@
 package helsinki.personnel;
 
+import static ua.com.fielden.platform.entity.query.fluent.EntityQueryUtils.expr;
+import static ua.com.fielden.platform.entity.query.fluent.EntityQueryUtils.select;
 import static ua.com.fielden.platform.reflection.TitlesDescsGetter.getEntityTitleAndDesc;
 
 import helsinki.security.tokens.persistent.Person_CanModify_user_Token;
 import ua.com.fielden.platform.entity.ActivatableAbstractEntity;
 import ua.com.fielden.platform.entity.DynamicEntityKey;
+import ua.com.fielden.platform.entity.annotation.Calculated;
 import ua.com.fielden.platform.entity.annotation.CompanionObject;
 import ua.com.fielden.platform.entity.annotation.CompositeKeyMember;
 import ua.com.fielden.platform.entity.annotation.DescRequired;
@@ -16,11 +19,13 @@ import ua.com.fielden.platform.entity.annotation.KeyType;
 import ua.com.fielden.platform.entity.annotation.MapEntityTo;
 import ua.com.fielden.platform.entity.annotation.MapTo;
 import ua.com.fielden.platform.entity.annotation.Observable;
+import ua.com.fielden.platform.entity.annotation.Readonly;
 import ua.com.fielden.platform.entity.annotation.SkipEntityExistsValidation;
 import ua.com.fielden.platform.entity.annotation.Title;
 import ua.com.fielden.platform.entity.annotation.Unique;
 import ua.com.fielden.platform.entity.annotation.mutator.BeforeChange;
 import ua.com.fielden.platform.entity.annotation.mutator.Handler;
+import ua.com.fielden.platform.entity.query.model.ExpressionModel;
 import ua.com.fielden.platform.property.validator.EmailValidator;
 import ua.com.fielden.platform.security.Authorise;
 import ua.com.fielden.platform.security.user.User;
@@ -37,20 +42,37 @@ import ua.com.fielden.platform.utils.Pair;
 @DescTitle(value = "Full Name", desc = "Person's full name - e.g. the first name followed by the middle initial followed by the surname.")
 @MapEntityTo
 @CompanionObject(PersonCo.class)
-@DescRequired
 @DisplayDescription
 public class Person extends ActivatableAbstractEntity<DynamicEntityKey> {
 
     private static final Pair<String, String> entityTitleAndDesc = getEntityTitleAndDesc(Person.class);
     public static final String ENTITY_TITLE = entityTitleAndDesc.getKey();
     public static final String ENTITY_DESC = entityTitleAndDesc.getValue();
-
+    
     @IsProperty
     @CompositeKeyMember(1)
     @MapTo
     @Title(value = "Email", desc = "Uniquely identifies a person.")
     @BeforeChange(@Handler(EmailValidator.class))
     private String email;
+    
+    @IsProperty
+    @MapTo
+    @Title(value = "First name", desc = "Respersents person's first name")
+    private String name;
+
+    @IsProperty
+    @MapTo
+    @Title(value = "Surname", desc = "Respersents person's second name")
+    private  String surname;
+
+    
+    @IsProperty
+    @Readonly
+    @Calculated
+    @Title(value = "Full name", desc = "Represents persons full name")
+    private String desc;
+    protected static final ExpressionModel desc_ = expr().concat().prop("name").with().val(" ").with().prop("surname").end().model();
 
     @IsProperty
     @Unique
@@ -82,8 +104,16 @@ public class Person extends ActivatableAbstractEntity<DynamicEntityKey> {
     @Override
     @Observable
     public Person setDesc(final String desc) {
-        return (Person) super.setDesc(desc);
+        this.desc = desc;
+        return this;
     }
+    
+    public String getDesc() {
+        return desc;
+    }
+
+ 
+
 
     @Observable
     public Person setEmail(final String email) {
@@ -157,5 +187,24 @@ public class Person extends ActivatableAbstractEntity<DynamicEntityKey> {
     public boolean isAUser() {
         return getUser() != null;
     }
+    
 
+    @Observable
+    public Person setName(final String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+    @Observable
+    public Person setSurname(final String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
 }
